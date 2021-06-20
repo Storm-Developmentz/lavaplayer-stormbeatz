f9f92d462d478460bb2d81cccc8b6fd38129d33d
Muhimur <muhimur10@gmail.com>
2021-06-20 00:59:54 -0400

    YT AGE RESTRICTED VIDEOS

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/DefaultYoutubeTrackDetailsLoader.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/DefaultYoutubeTrackDetailsLoader.java
@@ -214,8 +214,8 @@ public class DefaultYoutubeTrackDetailsLoader implements YoutubeTrackDetailsLoad
   protected JsonBrowser loadTrackArgsFromVideoInfoPage(HttpInterface httpInterface, String videoId, String sts) throws IOException {
     String videoApiUrl = "https://youtube.googleapis.com/v/" + videoId;
     String encodedApiUrl = URLEncoder.encode(videoApiUrl, UTF_8.name());
-    String url = "https://www.youtube.com/get_video_info?html5=1&video_id=" + videoId + "&eurl=https://youtube.googleapis.com/v/" + videoId +
-    "&c=TVHTML5&cver=6.20180913";
+    String url = "https://www.youtube.com/get_video_info?html5=1&video_id=" + videoId + "&eurl=" + encodedApiUrl +
+        "&c=TVHTML5&cver=7.20201028";
 
     if (sts != null) {
       url += "&sts=" + sts;
